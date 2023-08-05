94123e0ce40cb89f515f308fd3006d756f38c299
Jiuyang Liu <liu@jiuyang.me>
2023-08-05 23:04:29 +0800

    use nix to install dependencies

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "cde";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+    let
+      overlay = import ./overlay.nix;
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+        deps = with pkgs; [
+          git
+          mill
+        ];
+      in
+        {
+          legacyPackages = pkgs;
+          devShell = pkgs.mkShell {
+            buildInputs = deps;
+          };
+        }
+      )
+    // { inherit inputs; overlays.default = overlay; };
+}
---
+++ overlay.nix
@@ -0,0 +1,9 @@
+final: prev: {
+  mill = prev.mill.overrideAttrs (oldAttrs: rec {
+    version = "0.11.1";
+    src = prev.fetchurl {
+      url = "https://github.com/com-lihaoyi/mill/releases/download/${version}/${version}-assembly";
+      hash = "sha256-qG+Ddn0BHUZX1VX5hO84exgRz8YuUgYF/fH6MmgkrXE=";
+    };
+  });
+}
