f99dcb5b2aa122b082805c23515f053420da1b8b
Jiuyang Liu <liu@jiuyang.me>
2023-08-05 23:04:29 +0800

    use nix to install dependencies

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "cde";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+    let
+      overlay = import ./overlay.nix;
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+        deps = with pkgs; [
+          git
+          mill
+        ];
+      in
+        {
+          legacyPackages = pkgs;
+          devShell = pkgs.mkShell { };
+        }
+      )
+    // { inherit inputs; overlays.default = overlay; };
+}
